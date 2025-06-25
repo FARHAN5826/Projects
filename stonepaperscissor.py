9b17152a3130764a2bc6acb33b5cca79bd4dffd0
FARHAN5826 <161864798+FARHAN5826@users.noreply.github.com>
2025-06-25 21:23:51 +0530

    Update stonepaperscissor.py

--- stonepaperscissor.py
+++ stonepaperscissor.py
@@ -10,8 +10,10 @@ stone =0
 paper =1
 scissore =-1
 '''
+engine.say("Stone ,Paper ,Scissor ?")
+engine.runAndWait()
+youstr=input("Stone ,Paper ,Scissor ? \n")
 
-youstr=input("Stone ,Paper ,Scissor ? \n");
 youdic={"Stone" : 0,"Paper" : 1,"Scissor" : -1}
 revdic ={0 :"Stone",1 :"Paper",-1 :"Scissor"}
 you=youdic[youstr]
@@ -22,26 +24,44 @@ engine.say(pcstr)
 engine.runAndWait()
 
 print(f"You Chose {youstr} and Pc chose {pcstr}")
+engine.say(f"You Chose {youstr} and Pc chose {pcstr}")
+engine.runAndWait()
 
 if(you==computer):
     print("It is Draw")
+    engine.say("It is Draw ")
+    engine.runAndWait()
     
 else :
     if(you==0 and computer==1):
         print("You Lose")
+        engine.say("You Lose ")
+        engine.runAndWait()
 
     elif(you==0 and computer==-1):
         print("You Won")
+        engine.say("You Won ")
+        engine.runAndWait()
     elif(you==1 and computer==-1):
         print("You Lose")
+        engine.say("You Lose ")
+        engine.runAndWait()
     elif(you==1 and computer==0):
         print("You Won")
+        engine.say("You Won ")
+        engine.runAndWait()
     elif(you==-1 and computer==0):
         print("You Lose")
+        engine.say("You Lose ")
+        engine.runAndWait()
     elif(you==-1 and computer==1):
         print("You Won")
+        engine.say("You Won ")
+        engine.runAndWait()
     else:
         print("Something Went Wrong")
+        engine.say("Something Went Wrong")
+        engine.runAndWait()
 
 engine.say("Thanks for Playing With Me ")
 engine.runAndWait()
